2b89b7ecb4851dfc16b65fc54f3c5f67aaba02f3
kiranojhanp <kiranojha1226@gmail.com>
2020-05-13 14:03:33 +0545

    A basic restful API using mongoose ODM

--- index.js
+++ index.js
@@ -1,50 +1,96 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Task = require("./models/Task");
 
 const app = express();
 
+mongoose
+  .connect("mongodb://127.0.0.1:27017/taskManagerMarch2020", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Connected to database server");
+  });
+
 // for json support
 app.use(express.json());
-
-/* Simple restful API */
 
 app.get("/", (req, res) => {
   res.send("Welcome to my app");
 });
 
-app.get("/todo", (req, res) => {
-  res.send("Send all todos");
+/* Simple restful API */
+
+app.get("/task", (req, res) => {
+  Task.find()
+    .then((tasks) => {
+      res.status(201).json(tasks);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-app.post("/todo", (req, res) => {
-  console.log(req.body.desc);
-  
-  res.send("Should create a new todo");
+app.post("/task", (req, res) => {
+  Task.create(req.body)
+    .then((task) => {
+      res.status(201).json(task);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-app.put("/todo", (req, res) => {
+app.put("/task", (req, res) => {
   res.status(401).send("NOT SUPPORTED");
 });
 
-app.delete("/todo", (req, res) => {
-  res.send("Delete all todos");
+app.delete("/task", (req, res) => {
+  Task.deleteMany()
+    .then((reply) => {
+      res.json(reply);
+    })
+    .catch((err) => console.log(err));
 });
 
 /* Restful API with id */
 
-app.get("/todo/:id", (req, res) => {
-  res.send("Send a todo with id " + req.params.id);
+app.get("/task/:id", (req, res) => {
+  Task.findById(req.params.id)
+    .then((task) => {
+      res.status(201).json(task);
+    })
+    .catch((err) => console.log(err));
 });
 
-app.post("/todo/:id", (req, res) => {
+app.post("/task/:id", (req, res) => {
   res.status(401).send("NOT SUPPORTED");
 });
 
-app.put("/todo/:id", (req, res) => {
-  res.send(`Update the todo with id ${req.params.id}`);
+app.put("/task/:id", (req, res) => {
+  Task.findById(req.params.id)
+    .then((task) => {
+      if (req.body.name) {
+        task.name = req.body.name;
+      }
+
+      task.done = req.body.done;
+
+      task.save().then((updatedTask) => {
+        res.json(updatedTask);
+      });
+    })
+    .catch((err) => console.log(err));
 });
 
-app.delete("/todo/:id", (req, res) => {
-  res.send(`Deleted the todo with id ${req.params.id}`);
+app.delete("/task/:id", (req, res) => {
+  Task.findByIdAndRemove(req.params.id)
+    .then((reply) => {
+      res.json(reply);
+    })
+    .catch((err) => console.log(err));
 });
 
 app.listen(3000, () => {
